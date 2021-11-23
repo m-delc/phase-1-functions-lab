661e586fa0d8d11f6ae42d87d72c5930828f3f2c
MichaelDelcambre <delcambrem@gmail.com>
2021-11-23 08:45:42 -0600

    commit tree 57214489a55cb86221558b281120a9ec4e59ce3c

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+// const hq = 42
+// const num1 = 8
+
+// function distanceFromHqInBlocks(num1) {
+//     let result
+//     if (num1 > hq) {
+//         result = num1 - hq;
+//     }
+//     else if (num1 < hq) {
+//         result = hq - num1;
+//     }
+//     return result
+// }
+
+function distanceFromHqInBlocks(num1) {
+    const hq = 42;
+    const distanceToFortyTwo = Math.abs(num1 - hq);
+    return distanceToFortyTwo;
+}
+
+
+function distanceFromHqInFeet(num1) {
+    distanceFromHqInBlocks(num1);
+    const blocks = distanceFromHqInBlocks(num1) * 264;
+    return blocks;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const travel = (destination - start) * 264;
+    return Math.abs(travel);
+}
+
+
+// function distanceFromHqInFeet(feet){
+//     let result = num1 * 264
+//     return result
+// }
+
+
+
+
+
+
+
+// const distanceFromHqInFeet = function(distanceFromHqInBlocks) {
+//     return distanceFromHqInBlocks * 264
+// }
+
+// function distanceFromHqInFeet(someNum) {
+//     distanceFromHqInBlocks(num1) 
+//         let result
+//         if (num1 > hq) {
+//             result = (num1 - hq)*264;
+//         }
+//         else if (hq > num1) {
+//             result = (hq - num1)*264;
+//         }
+//         return result 
+// }
